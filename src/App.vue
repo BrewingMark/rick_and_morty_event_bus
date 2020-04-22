ae04b657d348953ae8b8a7b2d93421930e6d16cd
Mark Nicol <marknicol1991@live.co.uk>
2020-04-22 20:10:26 +0100

    MVP functionality complete sans styling.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <template lang="html">
-
+  <div>
+    <h1>Rick and Morty Characters</h1>
+    <div class="main-container">
+      <character-list :characters="characters"></character-list>
+      <character-detail v-if="selectedCharacter" :character="selectedCharacter"></character-detail>
+    </div>
+  </div>
 </template>
 
 <script>
+
+import CharacterList from './components/CharacterList.vue';
+import CharacterDetail from './components/CharacterDetail.vue';
+import {eventBus} from './main.js';
+
 export default {
+  name: 'app',
+  data(){
+    return {
+      characters: [],
+      selectedCharacter: null
+    }
+  },
+  mounted(){
+    fetch('https://rickandmortyapi.com/api/character/')
+    .then(result => result.json())
+    .then(characters => this.characters = characters.results)
+
+    eventBus.$on("character-selected", (character) => {
+      this.selectedCharacter = character;
+    })
+  },
+  components: {
+    "character-list": CharacterList,
+    "character-detail": CharacterDetail
+  }
 }
 </script>
 
---
+++ src/components/CharacterDetail.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div>
+    <h2>{{character.name}}</h2>
+    <img v-bind:src="character.image" alt="character image">
+    <h4>Species: {{character.species}}</h4>
+    <h4>Gender: {{character.gender}}</h4>
+    <h4>Origin: {{character.origin.name}}</h4>
+    <h4>Status: {{character.status}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'character-detail',
+  props: ['character']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
